b4f0078ad69621a5403d1d4a4b8f1a125fdf6abf
Igor <igor2000@inbox.ru>
2017-12-10 22:11:50 +0200

    Initial commit

---
+++ src/first/Test.java
@@ -0,0 +1,9 @@
+package first;
+
+/**
+ * Created by MM on 10.12.2017.
+ */
+public class Test {
+    int a;
+
+}
---
+++ src/third/Test.java
@@ -0,0 +1,8 @@
+package third;
+
+/**
+ * Created by MM on 10.12.2017.
+ */
+public class Test {
+    int a;
+}
